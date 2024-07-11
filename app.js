c0c452f9cd8a7955a8c46d8d7d1c908cbbda8ece
eduardo.brito <eduardo.brito@plannersistemas.com.br>
2024-07-11 14:32:27 -0300

    feat: mostrar sorteados na tela

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ function sortear() {
     sorteados.push(numero);
   }
 
-  console.log(`Números sorteados = ${sorteados}`)
+  let resultado = document.getElementById('resultado');
+  resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
 }
 
 function obterNumeroAleatorio(min, max) {
